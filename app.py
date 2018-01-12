caba959f7abc43c7b58944b42c879e5cb00666f6
Sergio MR <sergio_emr@outlook.com>
2018-01-12 14:12:41 -0600

    Prueba para Heroku

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+class Item(Resource):
+	def get(self,agem):
+		return {'item': agem}
+
+#Clave agem escobedo 19021
+api.add_resource(Item, '/item/AGEM=<int:agem>')
+app.run(port=5000, debug = True)
